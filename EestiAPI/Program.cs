1082107a833627e1afdc16d8f27b0c370d590b59
skathanas <32923297+skathanas@users.noreply.github.com>
2020-02-01 15:59:16 +0200

    Add files via upload

---
+++ EestiAPI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+using Newtonsoft.Json;
+
+namespace EestiAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to facts about Estonia!");
+            EstFacts();
+        }
+
+        public static void EstFacts()
+        {
+            string EstFactsUrl = "https://restcountries.eu/rest/v2/name/eesti";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(EstFactsUrl);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                var eeInfo = JsonConvert.DeserializeObject<List<info.Country>>(response);
+                foreach (var land in eeInfo)
+                {
+                    Console.WriteLine($"Namr: {land.Name}");
+                    Console.WriteLine($"Capital: {land.Capital}");
+                    Console.WriteLine($"Country code: {land.Cioc}");
+                    Console.WriteLine($"Region: {land.Region}");
+                    Console.WriteLine($"Population: {land.Population}");
+
+                    foreach (var lang in land.Languages)
+                    {
+                        Console.WriteLine($"Language:{lang.Name}");
+                    }
+                    foreach (var domeen in land.TopLevelDomain)
+                    {
+                        Console.WriteLine($"Domain:{land.TopLevelDomain[0]}");
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ EestiAPI/info.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Net;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy.Json;
+
+namespace EestiAPI
+{
+    class info
+        
+    {
+   
+                public class Language
+            {
+                public string Name { get; set; }
+            }
+
+            public class Country
+            {
+                public string Name { get; set; }
+
+                public string Capital { get; set; }
+
+                public string Region { get; set; }
+
+                public string Cioc { get; set; }
+
+                public int Population { get; set; }
+
+                public List<string> TopLevelDomain { get; set; }
+
+                public List<Language> Languages { get; set; }
+            }
+        }
+
+
+    }
+
+
+
+
+
